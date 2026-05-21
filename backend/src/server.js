d4f57f070a86d97db6a01013ed9d67b7af4911a2
Aaryan Bhople <aaryanbhople005@gmail.com>
2026-05-21 08:50:03 +0530

    setup fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -38,8 +38,8 @@ app.get('/', (req, res) => {
 });
 
 // Import Routes
-// const authRoutes = require('./routes/auth');
-// app.use('/api/auth', authRoutes);
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
